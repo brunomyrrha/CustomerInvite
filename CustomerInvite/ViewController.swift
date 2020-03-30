472ca30f8159b00aa3ff65ef2640763be8a36c43
Bruno Myrrha <brunomyrrha@icloud.com>
2020-03-30 20:23:09 +0100

    Initial Commit

---
+++ CustomerInvite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomerInvite
+//
+//  Created by Bruno Diniz on 30/03/2020.
+//  Copyright © 2020 Bruno Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
